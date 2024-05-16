cd7ec359a53fdba220a1d21bea7dff441a24654f
sfsajid91 <sfsajid91@gmail.com>
2024-05-16 22:14:23 +0600

    feat: ✨ added top loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} antialiased`}>{children}</body>
+            <body className={`${inter.className} antialiased`}>
+                <NextTopLoader height={4} showSpinner={false} />
+                {children}
+            </body>
         </html>
     );
 }
