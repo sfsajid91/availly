6ecf9888ec14bf14c795016f87cd76c6883317a6
sfsajid91 <sfsajid91@gmail.com>
2024-05-16 19:28:19 +0600

    chore: 🔨 removed unnecessary redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    redirects: async () => {
-        return [
-            // added redirect due to nextauth redirect is not working
-            // config auth.config.ts in pages section
-            {
-                source: '/api/auth/login',
-                destination: '/login',
-                permanent: true,
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
